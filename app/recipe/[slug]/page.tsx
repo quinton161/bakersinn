656517e61986ee28553fb8c6db10cafc700d4dca
quinton161 <quintonndlovu161@gmail.com>
2025-03-21 16:07:07 -0500

    updated all files

--- app/recipe/[slug]/page.tsx
+++ app/recipe/[slug]/page.tsx
@@ -1,15 +1,6 @@
 import React from 'react'
 import RecipeClient from './RecipeClient'
 
-interface PageProps {
-  params: {
-    slug: string;
-  };
-  searchParams?: { [key: string]: string | string[] | undefined };
-}
-
-async function RecipePage({ params, searchParams }: PageProps) {
+export default function RecipePage({ params }: { params: { slug: string } }) {
   return <RecipeClient slug={params.slug} />;
-}
-
-export default RecipePage; 
+} 
